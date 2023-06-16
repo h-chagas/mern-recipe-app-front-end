66055eb930189faea7528795ca46d1e96f66cfda
Hugo Chagas <hugochagasuk@gmail.com>
2023-06-16 09:18:10 +0100

    create function to logout user

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,20 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.png";
 import { useState } from "react";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom"
 
 export const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
+  const [cookies, setCookies] = useCookies(["access_token"]);
+  const navigate = useNavigate();
+
+  const logout = () => { //function to logout. Will set cookies to empty string, remove userID from local storage, and will navigate to /auth page
+    setCookies("access_token", "")
+    window.localStorage.removeItem("userID");
+    navigate("/auth")
+  }
+
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -38,9 +49,11 @@ export const Navbar = () => {
           </svg>
         </button>
 
-        <div className={`w-full md:block md:w-auto ${
+        <div
+          className={`w-full md:block md:w-auto ${
             navbar ? "p-8 md:p-0 block" : "hidden"
-        }`}>
+          }`}
+        >
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
@@ -70,13 +83,17 @@ export const Navbar = () => {
               </Link>
             </li>
             <li>
-              <Link
-                to="/auth"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                onClick={() => setNavbar(!navbar)}
-              >
-                Login/Register
-              </Link>
+              {!cookies.access_token ? ( //If logged in, will show Log out button, else will show Login/Register Link anchor
+                <Link
+                  to="/auth"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  Login/Register
+                </Link>
+              ) : (
+                <button onClick={logout}>Log out</button>
+              )}
             </li>
           </ul>
         </div>
