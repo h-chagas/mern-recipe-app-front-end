fb47d9e89584ea4166ac2acd9151e37f1c8b2d3e
Hugo Chagas <hugochagasuk@gmail.com>
2023-06-16 14:59:47 +0100

    fix to className

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -120,7 +120,7 @@ const Form = ({
         </div>
         <button
           type="submit"
-          class="bg-blue-500 mt-6 self-auto hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="bg-blue-500 mt-6 self-auto hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
           {label}
         </button>
