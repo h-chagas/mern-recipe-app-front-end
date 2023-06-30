cd6d48fe21898009e8ff7ea48df8df7faa15e5c7
Hugo Chagas <hugochagasuk@gmail.com>
2023-06-30 09:00:10 +0100

    add square brackets to _ variable

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -18,7 +18,7 @@ const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const [_, setCookies] = useCookies("access_token")
+  const [_, setCookies] = useCookies(["access_token"])
 
   const navigate = useNavigate();
 
